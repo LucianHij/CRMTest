549846f16db880dd36af5117457e15d163ef2949
Lucian Hij <lucian.hij@cultofcoders.com>
2018-06-06 15:21:01 +0300

    first commit

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,37 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+import '/imports/ui/layouts/app-body.js';
+import '/imports/ui/pages/app-not-found.js';
+import '/imports/ui/pages/app-timeline.js';
+
+FlowRouter.route('/', {
+  name: 'App_timeline',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_timeline' });
+  },
+});
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_notFound' });
+  },
+};
+
+AccountsTemplates.configureRoute('signIn', {
+  name: 'signin',
+  path: '/signin',
+});
+
+AccountsTemplates.configureRoute('signUp', {
+  name: 'join',
+  path: '/join',
+});
+
+AccountsTemplates.configureRoute('forgotPwd');
+
+AccountsTemplates.configureRoute('resetPwd', {
+  name: 'resetPwd',
+  path: '/reset-password',
+});
